50d030933a3fe1646133a874de1e6f1b18a37582
fabs-silva <fabianabz@yahoo.com>
2020-06-03 23:16:11 -0300

    Adding routes and controllers

--- Ecoleta/server/src/routes.ts
+++ Ecoleta/server/src/routes.ts
@@ -1,10 +1,16 @@
 import express from 'express';
 
+import ItemsController from './controllers/ItemsController';
+import PointsController from './controllers/PointsController';
+
 const routes = express.Router();
+const itemsController = new ItemsController();
+const pointsController = new PointsController();
 
-routes.get('/', (request, response) => {
+routes.get('/items', itemsController.index);
 
-    return response.json({ message: 'Hello World'});
-});
+routes.post('/points', pointsController.create);
+routes.get('/points', pointsController.index);
+routes.get('/points/:id', pointsController.show);
 
 export default routes;
